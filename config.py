4c9ef1a629b3db8fd5f1eb85bf3ea72a51e1124e
Cavalex <mateusmpta@gmail.com>
2022-11-14 22:27:54 +0100

    changed limit to 200

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ OBJECTS_FILE = 'database/known_objects.json'  # file that stores the known objec
 SYSTEM = platform.system().lower()  # our operating system
 
 AGENT_NAME = "Cavalex Node"
-LOG_LIMIT = 300
+LOG_LIMIT = 200
 # logging things
 logging.basicConfig(
     filename='logs.log',
