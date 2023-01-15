d2d99ecb47430f97a40e05e9357298708287612d
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2023-01-15 16:57:36 +0100

    🌋 Adding run modes to main

--- main.py
+++ main.py
@@ -12,21 +12,36 @@ from utility.other_utilities import *
 from database.UTXO import UTXO
 
 def main():
-    reset_objects_file() # makes it easier to debug fetching the chaintip
 
-    UTXO.clear()
+    mode = "testing" # "testing", "production" or "grading"
+
     UTXO.start_auto_save()
-    UTXO.save()
     ObjectHandler.start_auto_save()
+    TimeTracker.regularly_print_stats()
     
+    if mode == "grading":
+        reset_objects_file() # makes it easier to debug fetching the chaintip
+        KnownNodesHandler.clear_known_nodes()
+        UTXO.clear()
+        UTXO.save()
+    
+    if mode == "production":
+        KnownNodesHandler.load_known_nodes()
+        UTXO.load_from_file()
+        ObjectHandler.load_objects()
+        ObjectHandler.update_id_to_index()
+
+    if mode == "testing":
+        reset_objects_file()
+        KnownNodesHandler.add_node(BOOTSTRAP_NODE)
+        UTXO.clear()
+        UTXO.save()
+
 
     # Let user change agent_name if it is the default
     if MessageGenerator.agent_name == "THIS COULD BE YOUR NODE":
         MessageGenerator.agent_name = input("Enter a name for your node: ")
-    #    agent_name = input("Enter a name for your node: ")
 
-    KnownNodesHandler.clear_known_nodes()
-    #KnownNodesHandler.load_known_nodes()
     KnownNodesHandler.set_active_nodes()
 
     ObjectHandler.load_objects()
